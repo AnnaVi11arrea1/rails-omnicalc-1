3143ac66646d9e5aa53c1de5c81a17a5eeeaec9a
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2025-02-03 19:28:49 +0000

    fixed weird home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
-
+  def home
+    render({ :template => "pages_templates/home" })
+  end
 
   def square
     render({ :template => "pages_templates/square" })
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get("/", {:controller => "pages", :action => "home"})
 
   get("/square/new", { :controller => "pages", :action => "square" })
   get("/square/results", { :controller => "pages", :action => "square_result" })
